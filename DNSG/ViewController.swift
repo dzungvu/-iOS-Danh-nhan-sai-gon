94e85cfe0865c74564ab7cff6af5df6e7c42786d
Dzungvu <dzungvucs@gmail.com>
2018-07-29 09:46:01 +0700

    Initial Commit

---
+++ DNSG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DNSG
+//
+//  Created by The Dung on 7/29/18.
+//  Copyright © 2018 The Dung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
